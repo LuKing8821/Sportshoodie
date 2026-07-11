99df2129f747405f1101bc8674a23330df6cb6b1
LuKing <luking@LuKingdeMacBook-Air.local>
2026-07-11 16:25:10 +0800

    Revert "Build Yinshan custom T-shirts site"
    This reverts commit c66ced5cac6b69a6aa4397d1e8954ead25a79464.

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,11 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://vantawear-custom-hoodie.chinayinshan.chatgpt.site/sitemap.xml" };
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://vantawear-custom-hoodie.chinayinshan.chatgpt.site/sitemap.xml",
+  };
 }
